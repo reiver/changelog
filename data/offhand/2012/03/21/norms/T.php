1ee0bb4d8d1a5d0e10e1d7fc03cde734198e02ce
Charles <charles@reptile.ca>
2013-03-17 10:35:06 -0700

    transferred over offhanded stuff from SVN to GitHub

---
+++ data/offhand/2012/03/21/norms/T.php
@@ -0,0 +1,7 @@
+<?php
+
+$s = 'Some literature on social norms & morality seem to suggest that everyone is a "slave" to their social norms and everyone moralizes them. '
+   . 'Suspect those inclined towards libertarianism (systemizers?) are (much) less likely to moralize social norms (and disgust), compared to others.'
+   ;
+
+echo "\n\n";echo 'STRLEN: ';echo strlen($s);echo "\n\n";
